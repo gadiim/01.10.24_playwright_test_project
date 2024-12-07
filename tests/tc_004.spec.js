4d6d1068e8729b0909a73a8ff1118a1305bffe2b
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-12-07 02:23:36 +0200

    Update test:сlicks & test:shop

--- tests/tc_004.spec.js
+++ tests/tc_004.spec.js
@@ -1,14 +1,14 @@
-// const { test, expect } = require('@playwright/test');
-// const { HomePage } = require('../pages/homePage');
+const { test, expect } = require('@playwright/test');
+const { HomePage } = require('../pages/homePage');
 
-// test('Click all elements in main-menu', async ({ page }) => {
+test('Click all elements in main-menu', async ({ page }) => {
 
-//     const homePage = new HomePage(page);
+    const homePage = new HomePage(page);
 
-//     await homePage.navigate();
+    await homePage.navigate();
 
-//     for (let i = 0; i < homePage.menuItems.length; i++) {
-//         await homePage.clickMenuItem(i);
-//         await page.screenshot({ path: `screenshots/tc004_${i + 1}.png` });
-//     }
-// });
+    for (let i = 0; i < homePage.menuItems.length; i++) {
+        await homePage.clickMenuItem(i);
+        await page.screenshot({ path: `screenshots/tc004_${i + 1}.png` });
+    }
+});
--- tests/tc_005.spec.js
+++ tests/tc_005.spec.js
@@ -1,29 +1,29 @@
-// const { test } = require('@playwright/test');
-// const { HomePage } = require('../pages/homePage');
-// const { ProductPage } = require('../pages/productPage');
+const { test } = require('@playwright/test');
+const { HomePage } = require('../pages/homePage');
+const { ProductPage } = require('../pages/productPage');
 
-// test('Add book to cart and verify', async ({ page }) => {
+test('Add book to cart and verify', async ({ page }) => {
 
-//     const homePage = new HomePage(page);
-//     const productPage = new ProductPage(page);
-//     const redmineBooks = page.locator('h2:has-text("Redmine books")');
+    const homePage = new HomePage(page);
+    const productPage = new ProductPage(page);
+    const redmineBooks = page.locator('h2:has-text("Redmine books")');
 
-//     await homePage.navigate();
+    await homePage.navigate();
 
-//     await redmineBooks.scrollIntoViewIfNeeded();
+    await redmineBooks.scrollIntoViewIfNeeded();
 
-//     await productPage.clickBook();
+    await productPage.clickBook();
 
-//     await productPage.addToCart();
+    await productPage.addToCart();
 
-//     await productPage.verifyItemAddedToCart();
-//     await page.screenshot({ path: 'screenshots/tc005_cart_contains.png' });
+    await productPage.verifyItemAddedToCart();
+    await page.screenshot({ path: 'screenshots/tc005_cart_contains.png' });
 
-//     await productPage.hoverOnCartIcon();
-//     await productPage.clickCartIcon();
+    await productPage.hoverOnCartIcon();
+    await productPage.clickCartIcon();
 
-//     await productPage.clickToRemoveFromCart();
+    await productPage.clickToRemoveFromCart();
 
-//     await productPage.verifyItemRemovedFromCart();
-//     await page.screenshot({ path: 'screenshots/tc005_cart_empty.png' });
-// });
+    await productPage.verifyItemRemovedFromCart();
+    await page.screenshot({ path: 'screenshots/tc005_cart_empty.png' });
+});
